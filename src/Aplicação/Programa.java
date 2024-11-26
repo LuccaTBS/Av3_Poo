8691d6cc8565d6395c21789fc7688583461b93a5
LuccaTBS <luccatbsilvani@gmail.com>
2024-11-26 01:32:20 -0300

    Add comments Programa.java

--- src/Aplicação/Programa.java
+++ src/Aplicação/Programa.java
@@ -1,7 +1,7 @@
 package Aplicação;
 
 import java.util.ArrayList;
-import java.util.InputMismatchException;
+import java.util.InputMismatchException; //Trata entradas invalidas do usuario
 import java.util.Scanner;
 
 import entidades.Acessorios;
@@ -15,13 +15,17 @@ public class Programa {
 	public static void main(String[] args) {
 
 		// TODO Auto-generated method stub
+		//Inicializa uma lista de produtos da loja
 		ArrayList<Produto> produtos = new ArrayList<>();
+
+		//Variáveis auxiliares para instanciar produtos
 		double precoAux = 250;
 		int codigoAux = 123;
 		String nomeAux = "Elden Ring";
 		int quantidadeAux = 5;
 		String tipoMidia = "Física";
 
+		//Vai adicionar um jogo à lista de produtos
 		produtos.add(new Jogos(precoAux, codigoAux, nomeAux, quantidadeAux, tipoMidia));
 
 		precoAux = 300;
@@ -31,6 +35,7 @@ public class Programa {
 		int armazenamentoAux = 10;
 		String corAux = "Preto";
 
+		//Vai adicionar um console a lista de produtos
 		produtos.add(new Console(precoAux, codigoAux, nomeAux, quantidadeAux, armazenamentoAux, corAux));
 
 		precoAux = 130;
@@ -40,6 +45,7 @@ public class Programa {
 		corAux = "Preto";
 		String tipoAux = "Fone";
 
+		//Vai adicionar um acessorio a lista de produtos
 		produtos.add(new Acessorios(precoAux, codigoAux, nomeAux, quantidadeAux, corAux, tipoAux));
 
 		precoAux = 20;
@@ -76,12 +82,10 @@ public class Programa {
 
 		produtos.add(new Jogos(precoAux, codigoAux, nomeAux, quantidadeAux, tipoMidia));
 
+		//Inicializa a loja com os produtos, nome e valor inicial de crédito
 		Loja loja = new Loja(produtos, "VaporGames", precoAux * 100);
-		/*
-		 * Mensagem de bem vindo while{ Mostrar todos os itens Possibilidades a se fazer
-		 * Mostrar a lista de itens Comprar algum item vender algum item Adicionar Mais
-		 * credito à loja Sair
-		 */
+
+		//Variáveis e objetos auxiliares para controle
 		boolean rodarPrograma = true;
 		Jogos jogoAux;
 		Console consoleAux;
@@ -89,6 +93,8 @@ public class Programa {
 		Scanner sc = new Scanner(System.in);
 		int opcaoEscolhida;
 		int indexProduto;
+
+		// Loop principal do programa
 		while (rodarPrograma) {
 			System.out.println("\n\n---------Menu Principal---------\n\n");
 			System.out.print(loja.mostrarProdutos());
@@ -96,12 +102,15 @@ public class Programa {
 			System.out.println("Informe como Deseja prosseguir:\n\nComprar Algum produto (1)\nVender algum produto (2)"
 					+ "\nAdicionar Mais Créditos à loja (3)\nEncerrar o programa (4)\n Mostrar os produtos (5)");
 			try {
+				//Vai capturar a escolha feita pelo usuario
 				opcaoEscolhida = sc.nextInt();
 
 				if (opcaoEscolhida == 1) {
 					System.out.println("Comprar produto já existente(1)\nComprar Novo produto(2)");
 					opcaoEscolhida = sc.nextInt();
 					if (opcaoEscolhida == 1) {
+
+						//Opção de comprar produtos
 						System.out.print("\nCódigo do Produto a se comprar mais: ");
 						codigoAux = sc.nextInt();
 						System.out.print("\nQuantidade de Produtos a se comprar: ");
@@ -113,6 +122,8 @@ public class Programa {
 							System.out.println("Código de produto não encontrado ou Dinheiro Insuficiente");
 						}
 					} else if (opcaoEscolhida == 2) {
+
+						//Adição de novos produtos
 						System.out.print("\nCódigo do Produto a se comprar: ");
 						codigoAux = sc.nextInt();
 						if (loja.encontrarProduto(codigoAux) == -1) {
@@ -124,8 +135,11 @@ public class Programa {
 							System.out.print("Preço: ");
 							precoAux = sc.nextDouble();
 							System.out.print("Quantidade: ");
-							quantidadeAux = sc.nextInt();
+							quantidadeAux = sc.nextInt(); //Verifica o tipo de produto e adiciona à loja
+							
 							if (opcaoEscolhida == 1) {
+
+								//Identifica o tipo de midia, cor, armazenamento
 								System.out.println("Tipo de midia: Física (1), Digital (2)");
 								opcaoEscolhida = sc.nextInt();
 								if (opcaoEscolhida == 1) {
@@ -224,6 +238,8 @@ public class Programa {
 					}
 
 				} else if (opcaoEscolhida == 2) {
+
+					//Venda de produtos
 					System.out.print("Digite o Código do item que Pretende vender\nCódigo: ");
 					codigoAux = sc.nextInt();
 					if (loja.encontrarProduto(codigoAux) != -1) {
@@ -239,12 +255,14 @@ public class Programa {
 						System.out.println("Código inválido!\nRetornando ao menu Principal");
 					}
 				} else if (opcaoEscolhida == 3) {
+
+					//Adição de investimentos à loja
 					System.out.print("Digite quanto dinheiro deseja Investir na Loja: ");
 					precoAux = sc.nextDouble();
 					loja.setDinheiro(loja.getDinheiro()+precoAux);
 					System.out.println("Investimento Completo!");
 				} else if (opcaoEscolhida == 4) {
-					rodarPrograma = false;
+					rodarPrograma = false; // Encerrar o programa
 				} else if (opcaoEscolhida == 5) {
 
 				} else {
@@ -252,6 +270,7 @@ public class Programa {
 				}
 			} catch (InputMismatchException e) {
 
+				// Tratamento de entrada inválida
 				System.out.println("Valor Inválido, Voltando ao menu inicial");
 				try {
 					Thread.sleep(2000);
