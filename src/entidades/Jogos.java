3534def950c0d3a3ab4817d0d6990e8f874f04cf
LuccaTBS <luccatbsilvani@gmail.com>
2024-11-26 01:05:40 -0300

    Add comments  Jogos.java

--- src/entidades/Jogos.java
+++ src/entidades/Jogos.java
@@ -1,9 +1,12 @@
 package entidades;
 
+//Classe Jogos, ela estende a classe produtos
 public class Jogos extends Produto{
-	
+
+	//Atributos específicos de Jogos
 	private String tipoDeMidia;
 
+	//Metodos Getters e Setters 
 	public String getTipoDeMidia() {
 		return tipoDeMidia;
 	}
@@ -12,6 +15,7 @@ public class Jogos extends Produto{
 		this.tipoDeMidia = tipoDeMidia;
 	}
 
+	//Construtor da classe Jogos
 	public Jogos(Double preco, Integer codigo, String nome, Integer quantidade, String tipoDeMidia) {
 		super(preco, codigo, nome, quantidade);
 		this.tipoDeMidia = tipoDeMidia;
